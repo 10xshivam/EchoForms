a99b3cf015a28bf42bdbbb36ab80a67bb8f8c2a3
Shivam <kumarshivam3788@gmail.com>
2025-02-01 12:38:09 +0530

    Refactor Home page pricing section; update text colors and backgrounds for improved accessibility and visual contrast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -208,32 +208,32 @@ export default function Home() {
         </div>
       </div>
       <div className="flex flex-col justify-center items-center gap-10 pb-32">
-        <h2 className="text-6xl font-semibold tracking-tighter bg-gradient-to-b from-white to-white/70 bg-clip-text text-transparent">
+        <h2 className="text-6xl font-semibold tracking-tighter bg-gradient-to-b from-black to-black/70 dark:from-white dark:to-white/70 bg-clip-text text-transparent">
           Plans & Prices.
         </h2>
         <div className="flex justify-center items-center gap-10">
           {pricingPlans.map((plan) => (
-            <div key={plan.name} className=" bg-zinc-900 p-5 px-7 flex flex-col rounded-xl space-y-2">
-              <p className="text-lg tracking-tight font-medium mb-3 text-zinc-200/50">{plan.name}</p>
+            <div key={plan.name} className="bg-zinc-900/10 dark:bg-zinc-900 p-5 px-7 flex flex-col rounded-xl space-y-2">
+              <p className="text-lg tracking-tight font-medium mb-3 text-zinc-700 dark:text-zinc-200/50">{plan.name}</p>
               <p className="text-3xl font-bold !mb-2">
                 {plan.price}
               </p>
               <div className="w-full h-0.5 bg-white/10"/>
-              <p className="text-md tracking-tight font-medium mb-2 text-white/70">
+              <p className="text-md tracking-tight font-medium mb-2 text-black/70 dark:text-white/70">
                 Features
               </p>
               <div className="flex flex-col">
                 {plan.features.map((feature)=>(
               <div key={feature} className="flex items-center gap-1 mb-1">
                 <Check
-                  className="bg-white/10 rounded-full p-1 inline"
+                  className="bg-black/5 dark:bg-white/10 rounded-full p-1 inline"
                   size={15}
                 />
                 <span className="text-zinc-500 text-sm">{feature}</span>
               </div>
                 ))}
               </div>
-              <Button className="bg-zinc-400/10 hover:bg-zinc-400/30 text-white !mb-1">{plan.buttonText}</Button>
+              <Button className="bg-zinc-700 dark:bg-zinc-400/10 hover:bg-zinc-800 dark:hover:bg-zinc-400/30 text-white !mb-1">{plan.buttonText}</Button>
             </div>
           ))}
         </div>
