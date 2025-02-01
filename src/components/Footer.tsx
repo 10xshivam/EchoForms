52eb8c4cc1680b88fe24eb93f332d9acc8a898c3
Shivam <kumarshivam3788@gmail.com>
2025-02-01 13:10:13 +0530

    Refactor Footer component; update copyright text color for improved visibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,8 +7,8 @@ export default function Footer() {
   return (
     <div className="w-full h-16 border-t-2 dark:border-white/20  dark:bg-black/60 items-center flex px-7 justify-between z-50 max-md:px-5">
       <div className="flex justify-center items-center gap-0.5 text-white/50">
-        <Copyright className="w-5 max-md:mr-1 text-black dark:text-white"/>
-        <p className="text-sm max-md:text-[0.7rem] max-md:leading-3 text-black dark:text-white ">2025 EchoForms, All Rights Reserved.</p>
+        <Copyright className="w-5 max-md:mr-1 text-black dark:text-white/50"/>
+        <p className="text-sm max-md:text-[0.7rem] max-md:leading-3 text-black dark:text-white/50 ">2025 EchoForms, All Rights Reserved.</p>
       </div>
       <div className="flex justify-center items-center gap-3">
         <Link href={"https://github.com/shivam-tsx/Safe-Report"} className="text-black/60 dark:text-white/70 hover:text-black dark:hover:text-white">
