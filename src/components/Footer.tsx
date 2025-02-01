435aace2b0b977355b73191bec6eda3748458148
Shivam <kumarshivam3788@gmail.com>
2025-02-01 12:17:09 +0530

    Update copyright text in Footer component to reflect new branding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <div className="w-full h-16 border-t-2 border-white/20  bg-black/60 items-center flex px-7 justify-between z-50 max-md:px-5">
       <div className="flex justify-center items-center gap-0.5 text-white/50">
         <Copyright className="w-5 max-md:mr-1"  />
-        <p className="text-sm max-md:text-[0.7rem] max-md:leading-3">2025 Quick Report, All Rights Reserved.</p>
+        <p className="text-sm max-md:text-[0.7rem] max-md:leading-3">2025 EchoForms, All Rights Reserved.</p>
       </div>
 
       <div className="flex justify-center items-center gap-1">
