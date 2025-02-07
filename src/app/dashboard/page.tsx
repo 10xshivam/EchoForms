cb44dc634f21e6547d77cb80bb9a705b964ccca5
Shivam <kumarshivam3788@gmail.com>
2025-02-08 01:10:34 +0530

    Refactor Dashboard component: remove authentication logic, add sidebar options with icons, and integrate Separator component for layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,46 +1,57 @@
 'use client';
-import { useAuth, useUser } from '@clerk/nextjs'
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import { useClerk } from '@clerk/nextjs'
+import { Separator } from '@/components/ui/separator';
+import { ChartColumn, CircleFadingArrowUp, LibraryBig, Plus, TextQuote } from 'lucide-react';
+
+const options = [
+  {
+    icon:<LibraryBig/>,
+    name:"My Forms",
+    url:""
+  },
+  {
+    icon:<TextQuote/>,
+    name:"Responses",
+    url:""
+  },
+  {
+    icon:<ChartColumn/>,
+    name:"Analyze",
+    url:""
+  },
+  {
+    icon:<CircleFadingArrowUp/>,
+    name:"Upgrade",
+    url:""
+  },
+]
 
 export default function Dashboard() {
-  const { user } = useUser();
-  const { signOut } = useClerk()
-  const { isSignedIn } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (isSignedIn) {
-      router.push("/dashboard");
-    }
-  }, [isSignedIn, router]);
-
-  if (!isSignedIn) {
-    return <div className="w-full min-h-96 flex flex-col justify-center items-center">Loading...</div>;
-  }
 
   return (
-    <div className="w-full min-h-96 flex flex-col justify-center items-center">
-      <h1>Welcome, {user?.fullName}!</h1>
-      <div className="user-profile">
-        {user?.imageUrl && (
-          <Image
-            src={user.imageUrl} 
-            alt="Profile" 
-            className="profile-image"
-            width={100}
-            height={100}
-          />
-        )}
-        <div className="user-details ">
-          <p><strong>Name:</strong> {user?.fullName || 'Not provided'}</p>
-          <p><strong>Email:</strong> {user?.primaryEmailAddress?.emailAddress}</p>
-          <p><strong>Account Created:</strong> {user?.createdAt?.toLocaleDateString()}</p>
+    <div className="w-full min-h-screen flex p-4">
+      <div className='w-60 min-h-full bg-zinc-900 rounded-xl p-4 flex flex-col gap-3 mr-3'>
+        <div className='flex gap-3 items-center'>
+          <div className='border-2 rounded-lg p-1'>
+            <Image src="/Logo.png" alt="EchoForms" width={26} height={26} />
+          </div>
+          <span className="text-2xl font-bold text-black/70 dark:text-white/70 tracking-tighter">EchoForms</span>
+        </div>
+        <Separator/>
+        <div className='flex gap-2 items-center bg-zinc-500/40 hover:bg-zinc-500/20 cursor-pointer p-2 rounded-lg my-3' >
+          <Plus size={25} strokeWidth={2}/>
+          <span>Create a form</span>
+        </div>
+        <div className='flex flex-col gap-3'>
+          {options.map((option)=>(
+            <div key={option.name} className='flex gap-3 items-center hover:bg-zinc-500/20 p-2 cursor-pointer rounded-lg ' >
+            {option.icon}
+            <span>{option.name}</span>
+          </div>
+          ))}
         </div>
       </div>
-      <button onClick={() => signOut({ redirectUrl: '/sign-in' })}>Sign out</button>
+      <div></div>
     </div>
   );
 }
