ffc076d3c3eca559c7e652decd57d00641d1b8a0
Shivam <kumarshivam3788@gmail.com>
2025-02-13 17:50:32 +0530

    Enhance form creation and submission APIs to enforce user plan limits; track created forms and total submissions for users

--- src/app/api/forms/submit/route.ts
+++ src/app/api/forms/submit/route.ts
@@ -1,15 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/db";
-import { submissions, forms } from "@/db/schema";
+import { submissions, forms, users } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { writeFile } from "fs/promises";
 import path from "path";
+import { currentUser } from "@clerk/nextjs/server";
 
 interface SubmissionContent {
-  [key: string]: string | null; 
+  [key: string]: string | null;
 }
 
-export async function POST(req: NextRequest){
+export async function POST(req: NextRequest) {
   try {
     const formData = await req.formData();
     const { searchParams } = new URL(req.url);
@@ -17,6 +18,29 @@ export async function POST(req: NextRequest){
 
     if (!shareId) {
       return NextResponse.json({ success: false, error: "Missing form link" });
+    }
+
+    const _user = await currentUser();
+    if (!_user) {
+      return NextResponse.json({ error: "User not found" });
+    }
+
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, _user.id))
+      .limit(1);
+    if (!user[0])
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+    const isPro = user[0].plan === "pro";
+    const totalSubmissions = user[0].totalSubmissions ?? 0;
+
+    if (!isPro && totalSubmissions >= 100) {
+      return NextResponse.json(
+        { error: "Free plan submission limit reached. Upgrade to Pro!" },
+        { status: 403 }
+      );
     }
 
     const form = await db
@@ -31,7 +55,7 @@ export async function POST(req: NextRequest){
     const submissionContent: SubmissionContent = {};
 
     for (const [key, value] of formData.entries()) {
-      if (key === "formFields") continue; 
+      if (key === "formFields") continue;
       if (value instanceof Blob) {
         const fileName = `${Date.now()}_${value.name}`;
         const filePath = `/temp/${fileName}`;
@@ -50,9 +74,14 @@ export async function POST(req: NextRequest){
     });
 
     await db
-    .update(forms)
-    .set({ submissions: (form[0].submissions ?? 0) + 1 })
-    .where(eq(forms.id, form[0].id));
+      .update(forms)
+      .set({ submissions: (form[0].submissions ?? 0) + 1 })
+      .where(eq(forms.id, form[0].id));
+
+    await db
+      .update(users)
+      .set({ totalSubmissions: totalSubmissions + 1 })
+      .where(eq(users.id, user[0].id));
 
     return NextResponse.json({
       success: true,
