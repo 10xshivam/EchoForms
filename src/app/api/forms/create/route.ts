ee5ea3abbb242ca282fdd1b1e098468c27692952
Shivam <kumarshivam3788@gmail.com>
2025-02-13 17:39:17 +0530

    Enhance form creation API to enforce user plan limits; update createdForms count for users

--- src/app/api/forms/create/route.ts
+++ src/app/api/forms/create/route.ts
@@ -1,7 +1,8 @@
 import { db } from "@/db";
-import { forms } from "@/db/schema";
+import { forms, users } from "@/db/schema";
 import { generateAIContent } from "@/lib/generator";
 import { currentUser } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
@@ -9,9 +10,19 @@ export async function POST(req: NextRequest) {
         const { description } = await req.json();
         console.log(description);
 
-        const user = await currentUser();
-        if (!user) {
+        const _user = await currentUser();
+        if (!_user) {
             return NextResponse.json({ error: "User not found" });
+        }
+
+        const user = await db.select().from(users).where(eq(users.id, _user.id)).limit(1);
+        if (!user[0]) return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+        const isPro = user[0].plan === "pro";
+        const createdForms = user[0].createdForms ?? 0;
+
+        if (!isPro && createdForms >= 10) {
+            return NextResponse.json({ error: "Free plan limit reached. Upgrade to Pro!" }, { status: 403 });
         }
 
         const formContent = await generateAIContent(description);
@@ -21,9 +32,14 @@ export async function POST(req: NextRequest) {
         console.log(formContent);
 
         const form = await db.insert(forms).values({
-            ownerId: user.id,
+            ownerId: user[0].id,
             content: formContent
         }).returning();
+
+        await db
+        .update(users)
+        .set({ createdForms: createdForms + 1 })
+        .where(eq(users.id, user[0].id));
 
         return NextResponse.json({
             success: true,
