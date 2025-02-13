165f475b43f492eec81cf62f19cada57e34e6ae1
Shivam <kumarshivam3788@gmail.com>
2025-02-14 03:09:03 +0530

    Add whitespace for improved code readability in data.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
 "use client";
-// import { PinContainer } from "@/components/ui/3d-pin";
+import { PinContainer } from "@/components/ui/3d-pin";
 // import { BoxReveal } from "@/components/ui/box-reveal";
 import { Button } from "@/components/ui/button";
 import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
-// import { Input } from "@/components/ui/input";
+import { Input } from "@/components/ui/input";
 import { InteractiveGridPattern } from "@/components/ui/interactive-grid-pattern";
-// import { Label } from "@/components/ui/label";
+import { Label } from "@/components/ui/label";
 import { Spotlight } from "@/components/ui/spotlight-new";
-// import { Textarea } from "@/components/ui/textarea";
+import { Textarea } from "@/components/ui/textarea";
 import {
-  // ArrowRight,
   ArrowUpRight,
+  Bot,
+  Cable,
   ChartColumn,
+  ChartSpline,
   Check,
+  ChevronRight,
+  FileDown,
+  QrCode,
   Share2,
   Sparkles,
   SquarePen,
@@ -53,10 +58,49 @@ const howItWorks = [
   },
 ];
 
+const features = [
+  {
+    title: "AI-Powered Form Generation",
+    description:
+      "Create any form instantly with a single prompt. No manual setup—just describe your needs, and EchoForms builds it for you.",
+    icon: <Bot size={35} />,
+  },
+  {
+    title: "Customize Your Forms",
+    description:
+      "Customize fields, apply validations, and design visually appealing forms with a seamless user experience.",
+    icon: <Sparkles size={30} />,
+  },
+  {
+    title: "Instant Sharing & Embedding",
+    description:
+      "Get a unique shareable link or QR code upon creation. Easily embed forms on your website with a simple copy-paste.",
+    icon: <QrCode size={35} />,
+  },
+  {
+    title: "Real-Time Response Tracking",
+    description:
+      "Monitor submissions as they happen with detailed analytics. Receive instant email notifications for every new response.",
+    icon: <ChartSpline size={35} />,
+  },
+  {
+    title: "Seamless Data Export",
+    description:
+      "Download all responses in CSV format for easy reporting and analysis. Manage submissions efficiently in one place.",
+    icon: <FileDown size={35} />,
+  },
+  {
+    title: "Seamless Integrations",
+    description:
+      "Connect with Notion, Zapier, Google Sheets, and other tools for automated workflows.",
+    icon: <Cable size={35} />,
+  },
+];
+
 export default function Home() {
   return (
     <div className="relative w-full min-h-screen flex flex-col items-center pt-72">
-      <Navbar/>
+      <Navbar />
       <Spotlight />
       <InteractiveGridPattern className="top-5 -z-10" />
       <div className="flex flex-col justify-center items-center gap-5 pb-80">
@@ -98,84 +142,93 @@ export default function Home() {
           </HoverBorderGradient>
         </div>
       </div>
-      {/* <div className="w-full pt-80 pb-20 flex flex-col justify-center items-center gap-10">
-        <h2 className="text-[3.5rem] font-semibold tracking-tighter text-white/80 mb-5">
-          Why EchoForms <span className="text-zinc-600">?</span>
+      <div className="w-full flex flex-col justify-center items-center gap-10 pb-32 px-40">
+        <h2 className="text-6xl font-semibold tracking-tighter bg-gradient-to-b from-black to-black/70 dark:from-white dark:to-white/70 bg-clip-text text-transparent pb-1">
+          Why EchoForms ?
         </h2>
-        <div className="flex gap-52">
-          <div className="size-full max-w-2xl  py-10">
-            <BoxReveal duration={0.5}>
-              <h2 className="mt-[.5rem] text-2xl">
-                The Future of{" "}
-                <span className="text-zinc-500 font-medium">
-                  AI-Powered Form Building
-                </span>
-              </h2>
-            </BoxReveal>
-            <BoxReveal duration={0.5}>
-              <div className="mt-6">
-                <p className="text-lg">
-                  -&gt; Generate forms instantly using
-                  <span className="font-semibold text-zinc-500">
-                    {" "}
-                    AI prompts
-                  </span>
-                  . <br />
-                  -&gt; No coding, no drag-and-drop—just describe and create.{" "}
-                  <br />
-                  -&gt; 100% customizable and embeddable in any platform. <br />
-                </p>
-              </div>
-            </BoxReveal>
-            <BoxReveal duration={0.5}>
-              <Button className="mt-[1.6rem] bg-zinc-800 text-white h-11 hover:bg-zinc-900">
-                Check Demo
-              <ArrowRight className="ml-2" size={16} />
-              </Button>
-            </BoxReveal>
+        <div className="bg-zinc-900/60 w-full rounded-2xl p-10">
+          <div className="grid grid-cols-2 w-full pb-5">
+            <div>
+              <h3 className="text-5xl font-semibold tracking-tight bg-gradient-to-r dark:from-white dark:to-zinc-400 bg-clip-text text-transparent">
+                Features That Matter
+              </h3>
+              <p className="mt-4 text-zinc-500 max-w-6xl text-lg">
+                Build dynamic, high-performing forms effortlessly. From
+                collecting customer feedback to managing registrations,
+                EchoForms simplifies every step. Automate workflows, enhance
+                data accuracy, and ensure a seamless user experience—all while
+                integrating with the tools you rely on. Designed for speed,
+                flexibility, and efficiency, so you can focus on what truly
+                matters.
+              </p>
+              <button className="mt-[1.6rem] bg-zinc-800 text-white h-12 text-lg  hover:bg-zinc-900 rounded-lg px-7">
+                See Live Demo
+                <ChevronRight className="ml-3 inline" size={20} />
+              </button>
+            </div>
+            <div className="h-[400px] pt-10">
+              <PinContainer title="/echoforms.com">
+                <div className="flex basis-full flex-col p-2 tracking-tight text-slate-100/50 sm:basis-1/2 w-[22rem] items-center ">
+                  <h3 className="font-bold text-xl text-white/90">
+                    Feedback Form
+                  </h3>
+                  <h4 className="text-white/20 text-lg my-1 ">
+                    We value your feedback
+                  </h4>
+                  <div className="w-full max-w-md">
+                    <Label className="text-white/60">Name</Label>
+                    <Input
+                      type="text"
+                      className="border-white/20 bg-zinc-950 mb-3"
+                      placeholder="Enter your name"
+                      disabled
+                    />
+                    <Label className="text-white/60">Email</Label>
+                    <Input
+                      type="email"
+                      className="border-white/20 bg-zinc-950 mb-3"
+                      placeholder="Enter your email"
+                      disabled
+                    />
+                    <Label className="text-white/60">Feedback</Label>
+                    <Textarea
+                      className="border-white/20 bg-zinc-950 mb-4"
+                      placeholder="Write your feedback here"
+                      disabled
+                    />
+                    <Button
+                      type="submit"
+                      className="w-full bg-blue-600 text-white hover:bg-blue-700"
+                    >
+                      Submit
+                    </Button>
+                  </div>
+                </div>
+              </PinContainer>
+            </div>
           </div>
-          <div className="h-fit w-full flex items-center justify-center p-5">
-            <PinContainer title="/echoforms.com">
-              <div className="flex basis-full flex-col p-2 tracking-tight text-slate-100/50 sm:basis-1/2 w-[22rem] items-center ">
-                <h3 className="font-bold text-xl text-white/90">
-                  Feedback Form
-                </h3>
-                <h4 className="text-white/20 text-lg my-1 ">
-                  We value your feedback
-                </h4>
-                <div className="w-full max-w-md">
-                  <Label className="text-white/60">Name</Label>
-                  <Input
-                    type="text"
-                    className="border-white/20 bg-zinc-950 mb-3"
-                    placeholder="Enter your name"
-                    disabled
-                  />
-                  <Label className="text-white/60">Email</Label>
-                  <Input
-                    type="email"
-                    className="border-white/20 bg-zinc-950 mb-3"
-                    placeholder="Enter your email"
-                    disabled
-                  />
-                  <Label className="text-white/60">Feedback</Label>
-                  <Textarea
-                    className="border-white/20 bg-zinc-950 mb-4"
-                    placeholder="Write your feedback here"
-                    disabled
-                  />
-                  <Button
-                    type="submit"
-                    className="w-full bg-blue-600 text-white hover:bg-blue-700"
-                  >
-                    Submit
-                  </Button>
+          <div className="grid grid-cols-2 gap-5 pt-10">
+            {features.map((feature, index) => (
+              <div
+                key={index}
+                className="group relative flex overflow-hidden rounded-2xl bg-zinc-900/10 dark:bg-zinc-900 p-6 transition-all hover:bg-zinc-900/20 dark:hover:bg-zinc-800/80 max-md:p-5 gap-3"
+              >
+                <div className="flex px-5  items-center justify-center rounded-xl bg-zinc-500/10">
+                  {feature.icon}
+                </div>
+                <div>
+                  <h3 className="text-xl font-medium text-black dark:text-white mb-2">
+                    {feature.title}
+                  </h3>
+                  <p className="text-sm leading-tight text-zinc-500 dark:text-zinc-400 ">
+                    {feature.description}
+                  </p>
                 </div>
               </div>
-            </PinContainer>
+            ))}
           </div>
         </div>
-      </div> */}
+      </div>
       <div className="flex flex-col justify-center items-center gap-10 pb-32">
         <h2 className="text-6xl font-semibold tracking-tighter bg-gradient-to-b from-black to-black/70 dark:from-white dark:to-white/70 bg-clip-text text-transparent">
           How it works ?
@@ -184,7 +237,7 @@ export default function Home() {
           {howItWorks.map((step) => (
             <div
               key={step.step}
-              className="group relative overflow-hidden rounded-2xl bg-zinc-900/10 dark:bg-zinc-900 p-6 transition-all hover:bg-zinc-900/20 dark:hover:bg-zinc-800/80 max-md:p-5"
+              className="group relative overflow-hidden rounded-2xl bg-zinc-800/20 dark:bg-zinc-900 p-6 transition-all hover:bg-zinc-900/20 dark:hover:bg-zinc-800/80 max-md:p-5"
             >
               <div className="absolute inset-0 bg-gradient-to-b from-white/10 to-transparent opacity-0 transition-opacity group-hover:opacity-100" />
               <div className="flex gap-6 max-md:flex-col max-md:gap-2">
@@ -215,32 +268,37 @@ export default function Home() {
         </h2>
         <div className="flex justify-center items-center gap-10">
           {pricingPlans.map((plan) => (
-            <div key={plan.name} className="bg-zinc-900/10 dark:bg-zinc-900 p-5 px-7 flex flex-col rounded-xl space-y-2">
-              <p className="text-lg tracking-tight font-medium mb-3 text-zinc-700 dark:text-zinc-200/50">{plan.name}</p>
-              <p className="text-3xl font-bold !mb-2">
-                {plan.price}
+            <div
+              key={plan.name}
+              className="bg-zinc-900/10 dark:bg-zinc-900 p-5 px-7 flex flex-col rounded-xl space-y-2"
+            >
+              <p className="text-lg tracking-tight font-medium mb-3 text-zinc-700 dark:text-zinc-200/50">
+                {plan.name}
               </p>
-              <div className="w-full h-0.5 bg-white/10"/>
+              <p className="text-3xl font-bold !mb-2">{plan.price}</p>
+              <div className="w-full h-0.5 bg-white/10" />
               <p className="text-md tracking-tight font-medium mb-2 text-black/70 dark:text-white/70">
                 Features
               </p>
               <div className="flex flex-col">
-                {plan.features.map((feature)=>(
-              <div key={feature} className="flex items-center gap-1 mb-1">
-                <Check
-                  className="bg-black/5 dark:bg-white/10 rounded-full p-1 inline"
-                  size={15}
-                />
-                <span className="text-zinc-500 text-sm">{feature}</span>
-              </div>
+                {plan.features.map((feature) => (
+                  <div key={feature} className="flex items-center gap-1 mb-1">
+                    <Check
+                      className="bg-black/5 dark:bg-white/10 rounded-full p-1 inline"
+                      size={15}
+                    />
+                    <span className="text-zinc-500 text-sm">{feature}</span>
+                  </div>
                 ))}
               </div>
-              <Button className="bg-zinc-700 dark:bg-zinc-400/10 hover:bg-zinc-800 dark:hover:bg-zinc-400/30 text-white !mb-1">{plan.buttonText}</Button>
+              <Button className="bg-zinc-700 dark:bg-zinc-400/10 hover:bg-zinc-800 dark:hover:bg-zinc-400/30 text-white !mb-1">
+                {plan.buttonText}
+              </Button>
             </div>
           ))}
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
