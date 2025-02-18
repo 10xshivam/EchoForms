0fea45ac76224f9115c8373f42f963e9d9934de8
Shivam <kumarshivam3788@gmail.com>
2025-02-18 16:07:16 +0530

    Enhance FormResponses component; update styling for table headers and cells, improve layout, and ensure consistent design for response display

--- src/app/form/responses/[formId]/page.tsx
+++ src/app/form/responses/[formId]/page.tsx
@@ -93,7 +93,6 @@ export default function FormResponses() {
       </div>
     );
   }
-
   return (
     <div className="w-full min-h-screen flex-col relative pt-24 px-20 justify-center items-center">
       <div className="w-full px-5 absolute top-4 left-0 flex justify-between items-center">
@@ -116,42 +115,54 @@ export default function FormResponses() {
         <h1 className="text-5xl font-bold tracking-tight bg-gradient-to-b from-black to-black/50 dark:from-white dark:to-zinc-400 bg-clip-text text-transparent py-1">
           {form.content.formTitle}
         </h1>
-        <p className="text-gray-600 font-medium mb-6 mt-1 tracking-tight text-lg">
+        <p className="text-zinc-500 font-medium mb-4 mt-1 tracking-tight text-lg">
           {form.content.formHeading}
         </p>
-
         <div className="mb-4">
           <Button onClick={exportToExcel}>Export to Excel</Button>
         </div>
-        {responses.length > 0 ? (
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>ID</TableHead>
-                <TableHead>SUBMITTED AT</TableHead>
-                {responses[0] &&
-                  Object.keys(responses[0].content).map((key) => (
-                    <TableHead key={key}>{key.toLocaleUpperCase()}</TableHead>
-                  ))}
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {responses.map((response) => (
-                <TableRow key={response.id}>
-                  <TableCell>{response.id}</TableCell>
-                  <TableCell>
-                    {new Date(response.createdAt).toLocaleString()}
-                  </TableCell>
-                  {Object.values(response.content).map((value, index) => (
-                    <TableCell key={index}>{value}</TableCell>
-                  ))}
+        <div className="bg-zinc-500/10 dark:bg-zinc-800/30 rounded-lg p-6">
+          {responses.length > 0 ? (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="border bg-zinc-900/10 dark:bg-zinc-700/20">
+                    id
+                  </TableHead>
+                  <TableHead className="border bg-zinc-900/10 dark:bg-zinc-700/20">
+                    submittedAt
+                  </TableHead>
+                  {responses[0] &&
+                    Object.keys(responses[0].content).map((key) => (
+                      <TableHead
+                        key={key}
+                        className="border bg-zinc-900/10 dark:bg-zinc-700/20"
+                      >
+                        {key}
+                      </TableHead>
+                    ))}
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        ) : (
-          <p>No responses available for this form.</p>
-        )}
+              </TableHeader>
+              <TableBody>
+                {responses.map((response) => (
+                  <TableRow key={response.id}>
+                    <TableCell className="border">{response.id}</TableCell>
+                    <TableCell className="border">
+                      {new Date(response.createdAt).toLocaleString()}
+                    </TableCell>
+                    {Object.values(response.content).map((value, index) => (
+                      <TableCell key={index} className="border">
+                        {value}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          ) : (
+            <p>No responses available for this form.</p>
+          )}
+        </div>
       </div>
     </div>
   );
