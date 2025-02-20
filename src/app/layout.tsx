d20df87f0e8f795d1ac3d3e8c17ba54b370f1e29
Shivam <kumarshivam3788@gmail.com>
2025-02-21 02:34:31 +0530

    Add Vercel Analytics integration to track user interactions and performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from 'react-hot-toast';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
         <body className={`${inter.className} overflow-x-hidden`}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
+            <Analytics/>
             <Toaster />
           </ThemeProvider>
         </body>
